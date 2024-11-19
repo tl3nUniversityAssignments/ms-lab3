2dea024d7eba7342497877c9be92f365eb6ca17f
Vasyl Pavlyuchenko <vasyl.pavlyuchenko@gmail.com>
2024-11-19 18:04:36 +0200

    fixed computations for variant 4

--- test.py
+++ test.py
@@ -1,5 +1,7 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
+# Зчитуємо спостереження з файлу та перетворюємо їх у масив numpy
 with open('y9.txt') as file:
     observations = np.array([line.split() for line in file.readlines()], float).T
 
@@ -9,18 +11,19 @@ m2 = 28
 m3 = 18
 # c2, c4, m1 невідомі
 
-# Матриця утворена функціяєю чутливості, використовується для оцінки невідомих параметрів за відоми спостереженнями, на часовому інтервалі
+# Матриця утворена функцією чутливості, використовується для оцінки невідомих параметрів за відомими спостереженнями, на часовому інтервалі
 def computeSensitivityMatrix(parameters):
     c2, c4, m1 = parameters
     return np.array([
         [0, 1, 0, 0, 0, 0],
         [-(c2 + c1) / m1, 0, c2 / m1, 0, 0, 0],
         [0, 0, 0, 1, 0, 0],
-        [c2 / m1, 0, -(c2 + c3) / m2, 0, c3 / m2, 0],
+        [c2 / m2, 0, -(c2 + c3) / m2, 0, c3 / m2, 0],
         [0, 0, 0, 0, 0, 1],
         [0, 0, c3 / m3, 0, -(c4 + c3) / m3, 0]
     ])
 
+# Обчислюємо похідні параметрів
 def computeParameterDerivatives(states, parameters):
     c2, c4, m1 = parameters
  
@@ -50,43 +53,17 @@ def computeParameterDerivatives(states, parameters):
         [0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0]
     ])
-    """
-    derivatives_param0 = np.array([
-        [0, 0, 0, 0, 0, 0],
-        [- 1 / m1, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0]
-    ])
-
-    derivatives_param1 = np.array([
-        [0, 0, 0, 0, 0, 0],
-        [- 1 / m1, 0, 1 / m1, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0],
-        [1 / parameters[2], 0, -1 / parameters[2], 0, c3 / parameters[2], 0],
-        [0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0]
-    ])
-
-    derivatives_param2 = np.array([
-        [0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0],
-        [- parameters[1] / (parameters[2] ** 2), 0, (parameters[1] + c3) / (parameters[2] ** 2), 0, -c3 / (parameters[2] ** 2), 0],
-        [0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0]
-    ])
-    """
-
+    
     derivatives_param0 = derivatives_param0 @ states
     derivatives_param1 = derivatives_param1 @ states
     derivatives_param2 = derivatives_param2 @ states
     return np.array([derivatives_param0, derivatives_param1, derivatives_param2]).T
 
+# Обчислюємо динаміку моделі
 def computeModelDynamics(states, parameters):
     return computeSensitivityMatrix(parameters) @ states
 
+# Оптимізуємо параметри
 def optimizeParameters(initial_parameters, start_time, end_time, time_step, tolerance):
     time_points = np.linspace(start_time, end_time, int((end_time - start_time) / time_step + 1))
 
@@ -95,7 +72,8 @@ def optimizeParameters(initial_parameters, start_time, end_time, time_step, tole
         model_states = np.zeros_like(observations)
         model_states[0] = observations[0].copy()
 
-        # Інтегруємо модель за методом Рунге-Кутта
+        # Інтегруємо модель за методом Рунге-Кутта;
+        # знаходимо стани моделі у кожен часовий відрізок
         for i in range(1, len(time_points)):
             previous_state = model_states[i - 1]
             k1 = time_step * computeModelDynamics(previous_state, initial_parameters)
@@ -124,10 +102,10 @@ def optimizeParameters(initial_parameters, start_time, end_time, time_step, tole
         parameter_update = sensitivity_derivatives @ sensitivity_observations
         initial_parameters += parameter_update
 
-        if np.abs(parameter_update).max() < tolerance:
-            break
+        if np.abs((parameter_update)).max() < tolerance:
+            return initial_parameters, model_states
 
-    return initial_parameters
+    
 
 
 if __name__ == "__main__":
@@ -135,5 +113,16 @@ if __name__ == "__main__":
     start_time = 0 # початок інтервалу
     end_time = 50 # кінець інтервалу
     time_step = 0.2 # крок
-    tolerance = 0.001 # параметр точності
-    print("Solution is:", optimizeParameters(initial_guess, start_time, end_time, time_step, tolerance))
+    tolerance = 1e-15 # параметр точності
+
+    solution, model_states = optimizeParameters(initial_guess, start_time, end_time, time_step, tolerance)
+    print("Solution is:", solution)
+
+    # Графік спостережень vs моделі
+    time_points = np.linspace(start_time, end_time, len(observations))
+    plt.plot(time_points, observations.T[0], label='Observations')
+    plt.plot(time_points, model_states.T[0], label='Model')
+    plt.xlabel('Time')
+    plt.ylabel('State')
+    plt.legend()
+    plt.show()
